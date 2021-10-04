b3eafb214ea988166bbac933825bd525d42bcb59
Hyun <pinkiepies0912@gmail.com>
2021-10-04 14:38:09 +0900

    feature: Add fee, tax to ticker table

--- trading_db/rdb/stock/tickers.py
+++ trading_db/rdb/stock/tickers.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
 
 from trading_db.rdb.base import Model
 
@@ -7,5 +7,7 @@ class StockTicker(Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     ticker = Column(String(255), nullable=False)
     firm_id = Column(Integer, ForeignKey("firms.id"))
+    fee = Column(Float, nullable=False)
+    tax = Column(Float, nullable=False)
 
     __tablename__ = "tickers"
